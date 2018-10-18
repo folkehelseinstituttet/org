620349690576a571fcf501fb9516601e636b2d53
Richard White <r@rwhite.no>
2018-10-18 10:23:53 +0300

    commit

---
+++ R/aa_environments.r
@@ -0,0 +1,10 @@
+#' PROJ
+#' @export PROJ
+PROJ <- new.env(parent = emptyenv())
+
+#' CONFIG
+#' @export CONFIG
+CONFIG <- new.env(parent = emptyenv())
+CONFIG$ALLOW_FILE_MANIPULATION_FROM_INITIALISE_PROJECT <- FALSE
+CONFIG$USE_RCLONE <- FALSE
+CONFIG$FORCE_RCLONE_RESYNC <- FALSE
---
+++ R/onAttach.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage('PACKAGE: org')
+  packageStartupMessage('Version')
+  packageStartupMessage('Developed by Richard White, Norwegian Institute of Public Health')
+}
