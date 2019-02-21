b1fc84a90b8dc174b34ccbb28289202c6dced4a3
Richard White <w@rwhite.no>
2019-02-21 11:06:56 +0200

    removing

--- org.Rcheck/00_pkg_src/org/R/onAttach.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("PACKAGE: org")
-  packageStartupMessage("Version 2019.02.21 at 08:48")
-  packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
-}
--- org.Rcheck/org/NEWS.md
+++
@@ -1,4 +0,0 @@
-# org 2019.02.21
-
-- Submission to CRAN
-- Includes functions `AllowFileManipulationFromInitialiseProject` and `InitialiseProject`
