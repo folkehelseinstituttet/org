68fcdc966ffcb1d3a3e836eefe097ae3fb372e41
Richard White <w@rwhite.no>
2020-02-18 19:54:11 +0100

    raubreywhite -> folkehelseinstituttet

--- travis/drat.sh
+++ travis/drat.sh
@@ -6,11 +6,11 @@ addToDrat(){
   ## Set up Repo parameters
   git init
   git config user.name "Richard White"
-  git config user.email "r@rwhite.no"
+  git config user.email "w@rwhite.no"
   git config --global push.default simple
 
   ## Get drat repo
-  git remote add upstream "https://$GITHUB_PAT@github.com/raubreywhite/drat.git"
+  git remote add upstream "https://$GITHUB_PAT@github.com/folkehelseinstituttet/drat.git"
   git fetch upstream 2>err.txt
   git checkout gh-pages
 
