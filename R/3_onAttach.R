c92290f65a20e951eedacbb1db268eeec587031c
Richard Aubrey White <hello@rwhite.no>
2021-01-28 07:19:07 +0100

    jenkins

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,5 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("PACKAGE: org")
-  packageStartupMessage("Version 2020.05.25 at 12:01")
-  packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
+  packageStartupMessage(paste(
+    "org",
+    utils::packageDescription("org")$Version,
+    "https://folkehelseinstituttet.github.io/org"
+  ))
 }
